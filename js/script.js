fa50e9aeb7d9e6e76e59cd74fd503f2d7c98320e
Ryan Yang <javathon@gmail.com>
2018-11-03 03:56:31 -0700

    updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 function load(){
     getStories();
 }
-
 function getStories(){
-    
+    const url = `https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=${GOOGLE_API_KEY}`;
+    fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        document.getElementById('root').textContent = JSON.stringify(response);
+    });
 }
