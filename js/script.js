6cb618d7d52d8e8fd326171498c5d86d7a7062f3
Ryan Yang <javathon@gmail.com>
2018-11-03 12:55:09 -0700

    news parse done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let data;
+
 function load(){
     getStories();
 }
@@ -8,6 +10,30 @@ function getStories(){
         return response.json();
     })
     .then(function(response){
-        document.getElementById('root').textContent = JSON.stringify(response);
+        console.log(response);
+        data = response;
+        generateNewsCards(response);
     });
+}
+function generateNewsCards(data){
+    data.articles.forEach(e => {
+        generateCard(e);
+    });
+}
+function generateCard({title, content, url, urlToImage}){
+    let cardTemplate = `
+    <div class="card">
+        <div class="card-image">
+            <img src="${urlToImage}">
+        </div>
+        <div class="card-content">
+            <div class="card-title">${title}</div>
+            ${content}
+        </div>
+        <div class="card-action">
+            <a href="${url}">Article Link</a>
+        </div>
+    </div>
+    `;
+    document.getElementById('news-stories--holder').insertAdjacentHTML('beforeend' ,cardTemplate);
 }
