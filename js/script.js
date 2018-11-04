decd0b98de5e43a2a787991172ba4f4133dd1d4e
Ryan Yang <javathon@gmail.com>
2018-11-03 17:01:46 -0700

    server code

--- js/script.js
+++ js/script.js
@@ -1,51 +1,28 @@
 let data;
 
 function load(){
-    // getStories();
     navigator.geolocation.getCurrentPosition(function(position) {
-        console.log(position.coords.latitude, position.coords.longitude);
        getLocation(position.coords.latitude, position.coords.longitude);
     });
-    console.log('eg');
 
 }
-function getStories(){
-    const url = `https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=${NEWS_API_KEY}`;
-    fetch(url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(response){
-        data = response;
-        generateNewsCards(response);
-    });
-}
-
 function getLocation(lat, long){
+    const url = `localhost:8080/?id=location&lat=${lat}&long=${long}`;    
+    console.log(lat, long, url);
 
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${GOOGLE_GEOLOCATION_API_KEY}`;
     fetch(url)
     .then(function(response){
+        console.log('egere');
         return response.json();
     })
     .then(function(response){
-        // console.log(response);
-        getLocationNews(response['results'][0]['address_components'][1]['long_name']+ ",_" + response['results'][0]['address_components'][3]['long_name']);
+        console.log(response);
+        generateNewsCards(JSON.parse(response));
+    }).catch(function(error){
+        console.log(error);
     });
-    console.log('eg');
-
-
 }
-function getLocationNews(area){
-    const url = "http://eventregistry.org/api/v1/event?query=" + encodeURI(`{"$query":{"locationUri":"http://en.wikipedia.org/wiki/${area}"}}&action=getEvents&resultType=events&eventsSortBy=date&eventsCount=50&eventImageCount=1&storyImageCount=1&apiKey=b51f8466-38d9-4b10-93e2-34ddfba8d0a7`);
-    fetch(url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(reponse){
-        generateNewsCards(reponse['events']['results']);
-    });
-}function generateNewsCards(data){
+function generateNewsCards(data){
     data.forEach(e => {
         generateCard(e);
     });
